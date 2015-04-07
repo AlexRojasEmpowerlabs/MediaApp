ccd802457ebf0452dc9faccbb2b2bacd5538c46e
Alex Rojas <jose.rojas@empowerlabs.com>
2015-04-07 17:56:09 -0500

    start 2

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
   var module = angular.module('app', ['onsen']);
-
+var misDatos;
   module.controller('AppController', function($scope, $data) {
     $scope.doSomething = function() {
       setTimeout(function() {
@@ -10,13 +10,20 @@
     };
   });
 
-  module.controller('DetailController', function($scope, $data) {
-    $scope.item = $data.selectedItem;
+  module.controller('DetailController', function($scope) {
+    $scope.ons.notification.alert({message: ""+misDatos.url,title: "intellibanks"});
+    $scope.url = misDatos.url;
   });
 
-  module.controller('MasterController', function($scope, $data) {
-    $scope.items = $data.items;  
-    $scope.video=function(){
+  module.controller('MasterController', function($scope, $data,$http) {
+    $scope.item = $data.item;  
+    $http.get('http://empowerlabs.com/proyectos/trackersAPI/mblocs/todos.php').
+    success(function(data, status, headers, config) {
+  	//$scope.ons.notification.alert({message: ""+data.url,title: "intellibanks"});
+   misDatos=data;
+   $scope.item=misDatos;
+    $scope.video=function(item){
+    	//$scope.ons.notification.alert({message: ""+item.url,title: "intellibanks"});
     	$scope.ons.navigator.pushPage('page2.html', {title : "1"});}; 
     $scope.audio=function(){
     	$scope.ons.navigator.pushPage('page3.html', {title : "1"});}; 
@@ -24,34 +31,17 @@
     	$scope.ons.navigator.pushPage('page4.html', {title : "1"});};
     $scope.presentacion=function(){
     	$scope.ons.navigator.pushPage('page5.html', {title : "1"});};
+  }).
+  error(function(data, status, headers, config) {
+  	
+  });
     
   });
 
   module.factory('$data', function() {
       var data = {};
       
-      data.items = [
-          { 
-              title: 'Item 1 Title',
-              label: '4h',
-              desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          },
-          { 
-              title: 'Another Item Title',
-              label: '6h',
-              desc: 'Ut enim ad minim veniam.'
-          },
-          { 
-              title: 'Yet Another Item Title',
-              label: '1day ago',
-              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-          },
-          { 
-              title: 'Yet Another Item Title',
-              label: '1day ago',
-              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-          }
-      ]; 
+      data.item = [misDatos];
       
       return data;
   });
